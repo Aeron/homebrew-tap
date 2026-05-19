be3a8a8ed283c7876566e99816af1dfa8a4058ae
Eugene “Aeron” Glybin <aeron@aeron.cc>
2026-05-19 18:55:26 +0200

    Update fff-mcp.rb

--- Formula/fff-mcp.rb
+++ Formula/fff-mcp.rb
@@ -3,29 +3,29 @@ class FffMcp < Formula
   homepage "https://github.com/dmtrKovalenko/fff.nvim"
   license "MIT"
 
-  v = "0.7.1"
+  v = "0.8.1"
 
   on_macos do
     on_arm do
       url "https://github.com/dmtrKovalenko/fff.nvim/releases/download/v#{v}/fff-mcp-aarch64-apple-darwin"
-      sha256 "b01a1174c31286c354a9e291760d1fe832c970069fd4403a183845a2b1ab8f85"
+      sha256 "d6e8f368da148584533ad1021496d108db1b9ca1972a80656f1a3f8b93e4085d"
     end
 
     on_intel do
       url "https://github.com/dmtrKovalenko/fff.nvim/releases/download/v#{v}/fff-mcp-x86_64-apple-darwin"
-      sha256 "5765ae9c96add1d4a6c4b51b43a8acc896d8af42e9b7dd966d8814053d53df82"
+      sha256 "9e97c700da5eb3488cfe8912a598b235c8ea84b4cf1d8425b432d5b9776ea979"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dmtrKovalenko/fff.nvim/releases/download/v#{v}/fff-mcp-aarch64-unknown-linux-gnu"
-      sha256 "3d105f3b4be81e6a2964bb34c091873a40cfe7686fffd216cf9ee4d547e18ff1"
+      sha256 "225890157ddd27d3dd55772eb5bd14a98bf2a53082cc6b2d7923493fc31fffac"
     end
 
     on_intel do
       url "https://github.com/dmtrKovalenko/fff.nvim/releases/download/v#{v}/fff-mcp-x86_64-unknown-linux-gnu"
-      sha256 "668867c61b421d4a59a04bc64e7c99ab9b3834a5126696832da79cee24a3d9ac"
+      sha256 "7e1085aa6c23d2ea38ef1db5f6c01e0290b5c3049413e273e735ad9c498fe2a8"
     end
   end
 
