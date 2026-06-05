bb31e2a98c1f6adbf679a3c363ab16941f5822b3
Eugene “Aeron” Glybin <aeron@aeron.cc>
2026-06-05 12:26:00 +0200

    Update fff-mcp.rb

--- Formula/fff-mcp.rb
+++ Formula/fff-mcp.rb
@@ -5,29 +5,29 @@ class FffMcp < Formula
 
   # NOTE: the version is a dedicated keyword; Homebrew linter will not be happy
   # if we use it, so we use a different class variable instead
-  v = "0.8.4"
+  v = "0.9.1"
 
   on_macos do
     on_arm do
       url "https://github.com/dmtrKovalenko/fff/releases/download/v#{v}/fff-mcp-aarch64-apple-darwin"
-      sha256 "70749ff857309fc1182ce235326dfa4fda445b0766ed8b04306b0f86a5deef6e"
+      sha256 "cee75793fff67b25b95303c4708ec88b2c707f5ca51df5975a77330fc169f6ab"
     end
 
     on_intel do
       url "https://github.com/dmtrKovalenko/fff/releases/download/v#{v}/fff-mcp-x86_64-apple-darwin"
-      sha256 "8361812e0322ce410131340a5eb58bbe2e7740da58d021510bb705462d22b4f7"
+      sha256 "37b7d09098c5a1a0f036111e125885f58f3e3ec65d5eac7dd1f5f773407dd0c9"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dmtrKovalenko/fff/releases/download/v#{v}/fff-mcp-aarch64-unknown-linux-gnu"
-      sha256 "ccb63751ddcf10469ba32bdf965fb8c17a8777ccc28a24c57d1cee7dda01fab5"
+      sha256 "32cae6f55e0eed764d0ae150c2e6c9332608b131442b56a14d9afa9877294ef7"
     end
 
     on_intel do
       url "https://github.com/dmtrKovalenko/fff/releases/download/v#{v}/fff-mcp-x86_64-unknown-linux-gnu"
-      sha256 "ce3d6aeef2cfe634a26d39c5f2d5a598dcc8329d86a36b3cc44d257ab6cda153"
+      sha256 "df96f1a25112d83117909b215e5e4a98917ac885163b23147c1819c4700a4f21"
     end
   end
 
