f40b0c7b43d0bfdf481e64cb16590de1fa022e7f
Eugene “Aeron” Glybin <aeron@aeron.cc>
2026-06-21 11:46:26 +0200

    Update fff-mcp.rb

--- Formula/fff-mcp.rb
+++ Formula/fff-mcp.rb
@@ -3,6 +3,8 @@ class FffMcp < Formula
   homepage "https://github.com/dmtrKovalenko/fff"
   license "MIT"
 
+  deprecate! date: "2026-06-21", because: "it has been superseded by dmtrKovalenko/fff/fff-mcp"
+
   # NOTE: the version is a dedicated keyword; Homebrew linter will not be happy
   # if we use it, so we use a different class variable instead
   v = "0.9.5"
